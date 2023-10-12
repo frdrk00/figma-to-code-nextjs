ca5a80d1e2c0f13b338c9328abdee59d1af11875
frdrk00 <masterkooo@gmail.com>
2023-10-12 18:10:10 +0200

    Final

---
+++ app/orders.tsx
@@ -0,0 +1,285 @@
+import Image from 'next/image'
+
+const Orders = () => {
+  return (
+    <div className="grid gird-cols-6 col-span-2">
+      <div className="p-4 border-t border-gray-200">
+        <div className="flex justify-between pr-3">
+          <div className="flex flex-row space-x-4">
+            <div>
+              <Image
+                src="/images/order-thumbs/image-1.png"
+                width={62}
+                height={40}
+                className="rounded-md object-cover"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-gray-800 text-base font-medium leading-7 tracking-tight text-left">
+                The Lounge, Bar & Hotel near the Beach
+              </h1>
+              <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight font-poppins">
+                Hotel room in Tokyo, Japan
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="grid grid-cols-3 space-x-20">
+              <div className='pl-5'>
+                <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight text-center font-poppins">
+                  24.06.203
+                </p>
+              </div>
+              <div className="px-2.5 py-1 rounded-full bg-green-100">
+                <p className="text-xs font-medium leading-4 tracking-tight text-center text-green-800">
+                  Active
+                </p>
+              </div>
+              <div>
+                <a href="">
+                  <p className="text-[#04CEDB] font-normal text-sm leading-5 tracking-wide text-center font-poppins">
+                    View
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-4 border-t border-gray-200">
+        <div className="flex justify-between">
+          <div className="flex flex-row space-x-4">
+            <div>
+              <Image
+                src="/images/order-thumbs/image-2.png"
+                width={62}
+                height={40}
+                className="rounded-md object-cover"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-gray-800 text-base font-medium leading-7 tracking-tight text-left">
+                The Lounge, Bar
+              </h1>
+              <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight font-poppins">
+                Hotel room in Tokyo, Japan
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="grid grid-cols-3 space-x-14 ">
+              <div>
+                <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight text-center font-poppins">
+                  24.06.203
+                </p>
+              </div>
+              <div className="px-2.5 py-1 rounded-full bg-gray-100">
+                <p className="text-xs font-medium leading-4 tracking-tight text-center text-gray-800">
+                  Completed
+                </p>
+              </div>
+              <div>
+                <a href="">
+                  <p className="text-[#04CEDB] font-normal text-sm leading-5 tracking-wide text-center font-poppins">
+                    View
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-4 border-t border-gray-200">
+        <div className="flex justify-between">
+          <div className="flex flex-row space-x-4">
+            <div>
+              <Image
+                src="/images/order-thumbs/image-3.png"
+                width={62}
+                height={40}
+                className="rounded-md object-cover"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-gray-800 text-base font-medium leading-7 tracking-tight text-left">
+                The Lounge, Bar & Hotel near the Beach
+              </h1>
+              <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight font-poppins">
+                Hotel room in Tokyo, Japan
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="grid grid-cols-3 space-x-14 ">
+              <div>
+                <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight text-center font-poppins">
+                  24.06.203
+                </p>
+              </div>
+              <div className="px-2.5 py-1 rounded-full bg-gray-100">
+                <p className="text-xs font-medium leading-4 tracking-tight text-center text-gray-800">
+                  Completed
+                </p>
+              </div>
+              <div>
+                <a href="">
+                  <p className="text-[#04CEDB] font-normal text-sm leading-5 tracking-wide text-center font-poppins">
+                    View
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-4 border-t border-gray-200">
+        <div className="flex justify-between">
+          <div className="flex flex-row space-x-4">
+            <div>
+              <Image
+                src="/images/order-thumbs/image-4.png"
+                width={62}
+                height={40}
+                className="rounded-md object-cover"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-gray-800 text-base font-medium leading-7 tracking-tight text-left">
+                The Lounge, Bar
+              </h1>
+              <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight font-poppins">
+                Hotel room in Tokyo, Japan
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="grid grid-cols-3 space-x-14 ">
+              <div>
+                <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight text-center font-poppins">
+                  24.06.203
+                </p>
+              </div>
+              <div className="px-2.5 py-1 rounded-full bg-gray-100">
+                <p className="text-xs font-medium leading-4 tracking-tight text-center text-gray-800">
+                  Completed
+                </p>
+              </div>
+              <div>
+                <a href="">
+                  <p className="text-[#04CEDB] font-normal text-sm leading-5 tracking-wide text-center font-poppins">
+                    View
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-4 border-t border-gray-200">
+        <div className="flex justify-between">
+          <div className="flex flex-row space-x-4">
+            <div>
+              <Image
+                src="/images/order-thumbs/image-5.png"
+                width={62}
+                height={40}
+                className="rounded-md object-cover"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-gray-800 text-base font-medium leading-7 tracking-tight text-left">
+                The Lounge
+              </h1>
+              <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight font-poppins">
+                Hotel room in Tokyo, Japan
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="grid grid-cols-3 space-x-14 ">
+              <div>
+                <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight text-center font-poppins">
+                  24.06.203
+                </p>
+              </div>
+              <div className="px-2.5 py-1 rounded-full bg-gray-100">
+                <p className="text-xs font-medium leading-4 tracking-tight text-center text-gray-800">
+                  Completed
+                </p>
+              </div>
+              <div>
+                <a href="">
+                  <p className="text-[#04CEDB] font-normal text-sm leading-5 tracking-wide text-center font-poppins">
+                    View
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-4 border-t border-gray-200">
+        <div className="flex justify-between">
+          <div className="flex flex-row space-x-4">
+            <div>
+              <Image
+                src="/images/order-thumbs/image-6.png"
+                width={62}
+                height={40}
+                className="rounded-md object-cover"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-gray-800 text-base font-medium leading-7 tracking-tight text-left">
+                The Lounge, Bar & Hotel near the Beach
+              </h1>
+              <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight font-poppins">
+                Hotel room in Tokyo, Japan
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="grid grid-cols-3 space-x-16">
+              <div className='pl-3'>
+                <p className="text-gray-500 text-sm leading-5 font-normal tracking-tight text-center font-poppins">
+                  24.06.203
+                </p>
+              </div>
+              <div className="px-2.5 py-1 rounded-full bg-red-100">
+                <p className="text-xs font-medium leading-4 tracking-tight text-center text-red-800">
+                  Cancelled
+                </p>
+              </div>
+              <div>
+                <a href="">
+                  <p className="text-[#04CEDB] font-normal text-sm leading-5 tracking-wide text-center font-poppins">
+                    View
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Orders
